9f1c7193d958bb5665ee050debea830f8485aa0c
Damien RIDEREAU <damien.ridereau@systeme-u.fr>
2023-02-15 11:15:12 +0100

    initial commit

---
+++ playbook_test/pod_event.py
@@ -0,0 +1,16 @@
+from robusta.api import *
+
+
+@action
+def my_action(event: PodEvent):
+    # we have full access to the pod on which the alert fired
+    pod = event.get_pod()
+    pod_name = pod.metadata.name
+    pod_logs = pod.get_logs()
+    pod_processes = pod.exec("ps aux")
+
+    # this is how you send data to slack or other destinations
+    event.add_enrichment([
+        MarkdownBlock("*Oh no!* An alert occurred on " + pod_name),
+        FileBlock("crashing-pod.log", pod_logs)
+    ])
---
+++ tests/test_playbook_test.py
@@ -0,0 +1,5 @@
+from playbook_test import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
