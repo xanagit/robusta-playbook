54459ea035efe8610dc7648d3622b8c9a50c34c5
Damien RIDEREAU <damien.ridereau@systeme-u.fr>
2023-02-15 14:39:40 +0100

    feat: add callback action

--- iris_robusta_actions/pod_event.py
+++ iris_robusta_actions/pod_event.py
@@ -1,12 +1,34 @@
 from robusta.api import *
 
 
+class CallbackParams():
+    title: str
+
+
 @action
-def iris_custom_action(event: ExecutionBaseEvent):
+def iris_custom_callback(event: ExecutionBaseEvent, params: CallbackParams):
+    finding = Finding(
+        title="Action callback",
+        source=FindingSource.PROMETHEUS,
+        aggregation_key="iris_custom_callback",
+    )
+    finding.add_enrichment(
+        [MarkdownBlock(f"Callback params : *{params.title}*")])
+    event.add_finding(finding)
+
+
+@action
+def iris_custom_action(alert: PrometheusKubernetesAlert):
     # pod = event.get_pod()
-    event.add_enrichment([
+    alert_name = alert.alert.labels.get("alertname", "")
+    if not alert_name:
+        return
+
+    alert.add_enrichment([
+        HeaderBlock("IRIS Custom Paybook action"),
         MarkdownBlock("*Une erreur est survenue!*"),
-        FileBlock("crashing-pod.log", "test de fichier de log")
+        DividerBlock(),
+        FileBlock("crashing-pod.log", str.encode("test de fichier de log"))
     ])
     # we have full access to the pod on which the alert fired
     # pod = event.get_pod()
